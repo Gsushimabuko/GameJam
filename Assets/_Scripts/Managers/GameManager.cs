1fc075a8391b3f402016cfe49cda31cdd962619d
Gabriel Shimabuko <20171470@aloe.ulima.edu.pe>
2022-09-25 20:21:51 -0500

    a

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -44,7 +45,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //SOFT RESET
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     private void rollEvent()
