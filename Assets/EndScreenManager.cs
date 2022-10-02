ca093c7895c7051ae86078e486f2fe9e9f678dec
Gabriel Shimabuko <20171470@aloe.ulima.edu.pe>
2022-10-02 17:38:53 -0500

    primer final terminado

--- Assets/EndScreenManager.cs
+++ Assets/EndScreenManager.cs
@@ -5,21 +5,75 @@ using UnityEngine.UI;
 
 public class EndScreenManager : MonoBehaviour
 {
-    public CanvasGroup canvasGroup;
-    private bool fadeIn = false;
-    private bool fadeOut = false;
+    public int currentState = 1;
+    public GameManager gameManager;
+    public Animator endingAnimator;
+    public DataManager eventScript;
+    public GameObject statPanel;
+    public TMPro.TextMeshProUGUI hapinessUI;
+    public TMPro.TextMeshProUGUI routeUI;
+    public TMPro.TextMeshProUGUI workUI;
+    public TMPro.TextMeshProUGUI socialUI;
+    public TMPro.TextMeshProUGUI moneyUI;
 
-    public void ShowUI()
+
+    public void nextAnimation()
     {
-        fadeIn = true; 
-    }
-    public void HideUI()
-    {
-        fadeOut = true;
+        currentState++;
+        endingAnimator.SetInteger("state", currentState);
     }
 
+    /*  public CanvasGroup canvasGroup;
+      private bool fadeIn = false;
+      private bool fadeOut = false;*/
+
+    /*   public void ShowUI()
+       {
+           fadeIn = true; 
+       }
+       public void HideUI()
+       {
+           fadeOut = true;
+       }
+   */
+
+    void Start()
+    {
+        
+        hapinessUI.text  = gameManager.hapiness.ToString();
+        workUI.text = gameManager.work.ToString();
+        socialUI.text = gameManager.socials.ToString();
+        moneyUI.text = gameManager.money.ToString();
+        //bug
+        if (eventScript._treeIndex == 0)
+        {
+            routeUI.text = "???: no llegaste a ninguna ruta";
+        }
+        else if( eventScript._eventIndex  == 1)
+        {
+            routeUI.text = "???: no llegaste a ninguna ruta";
+        }
+        //resultados esperados
+        else if( eventScript._eventIndex  == 2)
+        {
+            routeUI.text = "Manyado: no eres el público objetivo de este juego, pero gracias por jugar";
+        }
+        else if (eventScript._eventIndex == 3)
+        {
+            routeUI.text = "Persona normie: no desbordas personalidad";
+        }
+        else if (eventScript._eventIndex == 4)
+        {
+            routeUI.text = "Weeb: báñate";
+        }
+    }
     void Update()
     {
+        if(currentState >= 3)
+        {
+            statPanel.SetActive(true);
+        }
+        /*
         if (fadeIn)
         {
             if(canvasGroup.alpha < 1)
@@ -44,7 +98,7 @@ public class EndScreenManager : MonoBehaviour
                 }
 
             }
-        }
+        }*/
     }
 }
 
